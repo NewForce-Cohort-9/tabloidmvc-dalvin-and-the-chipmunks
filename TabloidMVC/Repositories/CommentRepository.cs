d100c6c21eb1a85020e97b87adf444fe93f67d77
Iris Lee <irislee4git@gmail.com>
2024-08-29 11:54:48 -0400

    list comments

--- TabloidMVC/Repositories/CommentRepository.cs
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -13,9 +13,35 @@ namespace TabloidMVC.Repositories
     {
         public CommentRepository(IConfiguration config) : base(config) { }
 
-        public List<Comment> GetAllComments()
+        public List<Comment> GetAllCommentsByPost(int PostId)
         {
-            
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT c.Id, c.PostId, c.UserProfileId, c.Subject, c.Content, 
+                              c.CreateDateTime,
+                              u.DisplayName,  
+                         FROM Comment c
+                              LEFT JOIN UserProfile u ON c.UserProfileId = u.id
+                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()
+                               AND c.PostId = PostId";
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Post>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(NewPostFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
         }
         
     }
