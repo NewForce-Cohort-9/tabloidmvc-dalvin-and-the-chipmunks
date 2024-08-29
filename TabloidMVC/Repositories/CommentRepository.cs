1df35f06b4d8c0985d046549b5dff1e39b588b21
Iris Lee <irislee4git@gmail.com>
2024-08-29 11:22:14 -0400

    comment set up

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public List<Comment> GetAllComments()
+        {
+            
+        }
+        
+    }
+}
