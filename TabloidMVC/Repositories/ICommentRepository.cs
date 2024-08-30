55918354faaa5791e87adc90ce76d5addc546775
Iris Lee <irislee4git@gmail.com>
2024-08-30 13:42:52 -0400

    add add method to comment repo

--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -6,5 +6,6 @@ namespace TabloidMVC.Repositories
     public interface ICommentRepository
     {
         List<Comment> GetAllCommentsByPost(int PostId);
+        void Add(Comment comment);
     }
 }
